429bd3c4e2d95e68c8dd24a8e102960bf36e5f62
Jenya <78879772+JenyaNoda@users.noreply.github.com>
2023-02-16 15:05:45 +0100

    Add files via upload

---
+++ 06.js
@@ -0,0 +1,21 @@
+// Setup
+function testStrict(val) {
+    if (val === 7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
+
+  // Setup
+function compareEquality(a, b) {
+    if (a === b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
+
+  
---
+++ 07.js
@@ -0,0 +1,18 @@
+function testNotEqual(val) {
+    if (val != 99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);
+
+  // Setup
+function testStrictNotEqual(val) {
+    if (val !== 17) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(10);
