a9f54a73a32329f272f8763c2bc43fbbe1afda83
Jenya <78879772+JenyaNoda@users.noreply.github.com>
2023-02-19 20:40:25 +0100

    Add files via upload

---
+++ 16.js
@@ -0,0 +1,18 @@
+const myDog = {
+    // Only change code below this line
+    "name": "pero",
+    "legs": 4,
+    "tails": 3,
+    "friends": ["Other Dogs"]
+  // Only change code above this line
+  };
+  // Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat
+const shirtValue = testObj.shirt  
---
+++ 18.js
@@ -0,0 +1,29 @@
+// Setup
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line
+  const playerNumber = 16;  
+  const player = testObj[playerNumber]
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog.name = "Happy Coder"
+  //
+  const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  }; 
+  
+  myDog.bark = "woof"
+    
