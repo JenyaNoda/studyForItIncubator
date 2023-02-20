5e963086412168b08549afa393a92effde37717c
Jenya <78879772+JenyaNoda@users.noreply.github.com>
2023-02-20 18:48:17 +0100

    Add files via upload

---
+++ 21.js
@@ -0,0 +1,13 @@
+// Setup
+const myArray = [];
+let i = 5; 
+while (i >= 0) {
+  myArray.push(i)
+  i--;
+  console.log(i)
+}
+const myArray = [];
+for (let i = 1; i < 6; i++) {
+  myArray.push(i)
+  console.log(i)
+}
