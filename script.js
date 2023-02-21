6731c7f3fa22e8ce3a486c5be749075585a60424
Jenya <78879772+JenyaNoda@users.noreply.github.com>
2023-02-21 19:14:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+ /*
+ let country = "Spain";
+let continent = "Europe";
+let population = 450;
+console.log(country)
+console.log(continent)
+console.log(population)
+
+let javascriptIsFun = true ;
+console.log(javascriptIsFun)
+console.log(typeof true)
+
+javascriptIsFun = "Yes";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+year = 1997;
+
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas,ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// 2 ** 3 meanns 2 to the power pf 3 + 2 * 2 * 2
+const firstName = "Jonas "
+const lastName = 'maan'
+console.log(firstName + lastName)
+// assigment operators 
+let x = 10  + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4 // x = x * 4 = 
+x++; // x = x + 1
+x--; // x = x - 1 
+console.log(x)
+console.log(ageJonas > ageSarah) // > , < , >=. =>
+console.log(ageJonas >=  18)
+*/
+
+// Comparison operators 
+
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991, now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5 // x = y = 10, x = 10
+console.log(x,y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
+
