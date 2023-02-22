9d0d82918be73808ba39ba5e99c81108862385dc
Jenya <78879772+JenyaNoda@users.noreply.github.com>
2023-02-22 22:06:02 +0100

    Add files via upload

---
+++ fundamentals js udemy course part1.js
@@ -0,0 +1,92 @@
+ /*
+ let country = "Spain";
+let continent = "Europe";
+let population = 450;
+console.log(country)
+console.log(continent)
+console.log(population)
+
+let javascriptIsFun = true ;
+console.log(javascriptIsFun)
+console.log(typeof true)
+
+javascriptIsFun = "Yes";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+year = 1997;
+
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas,ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// 2 ** 3 meanns 2 to the power pf 3 + 2 * 2 * 2
+const firstName = "Jonas "
+const lastName = 'maan'
+console.log(firstName + lastName)
+// assigment operators 
+let x = 10  + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4 // x = x * 4 = 
+x++; // x = x + 1
+x--; // x = x - 1 
+console.log(x)
+console.log(ageJonas > ageSarah) // > , < , >=. =>
+console.log(ageJonas >=  18)
+
+
+// Comparison operators 
+
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991, now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5 // x = y = 10, x = 10
+console.log(x,y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
+
+
+//  const dolphins = (96 + 108 + 89) / 3
+//  const koalas = (916 + 108 + 89) / 3
+//  if (dolphins > koalas) {
+//     console.log("DOlphins won")
+//  } else if 
+//    (dolphins === koalas) {
+//     console.log ('Draw')
+//  } else console.log('koalas won')
+
+const scoreDolphins = (97 + 112 + 101) / 3
+const scoreKoalas = (109 + 95 + 123) / 3
+console.log(scoreDolphins,scoreKoalas)
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log("Dolphins win the trophy")
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+    console.log("Koalas win the trophy")
+} else if (scoreDolphins === scoreKoalas) {
+    console.log("both win a trophy")
+} 
+*/
+
+// const age = 19;
+// const drink = age >= 18 ? 'wine' : 'water'
+// console.log(drink)
+
+// let drink2 
+// if (age >= 18) {
+//     drink2 = "wine"
+// } else {
+//     drink2 = "water"
+// } console.log(drink2) 
+// console.log (`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+
+// const tip = 301
+// const bill = 275
+// const calc = tip >= 50  &&  300 >= tip ? "15 prec": "is 20 prec"
+// console.log (`The bill was ${bill} and tip was ${tip} and the total value is ${tip + bill}`)
