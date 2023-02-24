dd993715e9cb741b5991368fa85b0c90c38301aa
Jenya <78879772+JenyaNoda@users.noreply.github.com>
2023-02-24 03:12:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -22,7 +22,9 @@
 
 // const appleOrangeJuice = fruitProccesor(2, 4) 
 // console.log(appleOrangeJuice)
-   //   function calcAge1(birthYear) {
+   
+
+//   function calcAge1(birthYear) {
    //      return 2037 - birthYear
    //   }
    //   const age1 = calcAge1(1997); 
@@ -47,14 +49,205 @@
    //  console.log(yearUntilRetirement(1997, 'Jenia')) 
    //  console.log(yearUntilRetirement(1978, 'Viktoria')) 
 
-   function cutFritesPieces(fruit) {
-      return fruit * 4 ;
-   }
-   function fruitProccesor(apples, oranges) {
-     const applePieces =  cutFritesPieces(apples);
-     const orangePieces = cutFritesPieces(oranges)
+
+   // function cutFritesPieces(fruit) {
+   //    return fruit * 4 ;
+   // }
+   // function fruitProccesor(apples, oranges) {
+   //   const applePieces =  cutFritesPieces(apples);
+   //   const orangePieces = cutFritesPieces(oranges)
          
-         const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of oranges`;
-         return  juice;
-      }
-      console.log(fruitProccesor(2, 3));
+   //       const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of oranges`;
+   //       return  juice;
+   //    }
+   //    console.log(fruitProccesor(2, 3));
+
+ 
+   // const calcAge1 = year => 2030 - year;
+   // const year1 = calcAge1(3)
+   // console.log(`The Lust Of Us 3 should be realeased in ${year1} `);     
+ 
+   // const calcAge = function(birthYear) {
+   //    return 2037 - birthYear;
+   // }
+
+   //   const yearUntilRetirement = function(birthYear, firstName) {
+   //    const age = calcAge(birthYear);
+   //    const retirementAge = 65 - age
+   //    if (retirementAge > 0) {
+   //       return retirementAge;
+   //    } else {
+   //    return -1;
+   // }
+   //    return retirementAge;
+
+   //    // return `${firstName} retires in ${retirementAge} years`
+   //   } 
+   //   console.log(yearUntilRetirement(1997, 'Jenia'));
+   //   console.log(yearUntilRetirement(1970, 'mIKEY'));
+   'use strict';
+// let hasDriverlicense = false;
+// const passTest = true;
+
+// if(passTest) hasDriverlicense = true;
+// if (hasDriverlicense) 
+// console.log('i can drive')
+
+// FUNCTIONS STUDY 
+
+// function logger(){
+//     console.log('My name is jenia')
+// }
+// logger();
+// function fruitProccesor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//     return  juice;
+// }
+// const appleJuice = fruitProccesor(5, 0)
+// console.log(appleJuice)
+
+// const appleOrangeJuice = fruitProccesor(2, 4) 
+// console.log(appleOrangeJuice)
+   //   function calcAge1(birthYear) {
+   //      return 2037 - birthYear
+   //   }
+   //   const age1 = calcAge1(1997); 
+   //   console.log(age1); 
+
+   //   const calcAge2 = function(birthYear) {
+   //      return 2037 - birthYear;
+   //   }
+   //   const age2 = calcAge2(2004)
+   //   console.log(age1, age2); 
+     // arrow function 
+    //  birthYear => 2037 - birthYear
+    //  const calcAge3 = birthYear => 2037 - birthYear
+    //  const age3 = calcAge3(2037)
+    //  console.log(age3)
+    //  const yearUntilRetirement = (birthYear, firstName) => {
+    //   const age = 2023 - birthYear
+    //   const retirementAge = 65 - age
+    //   // return retirement
+    //   return `${firstName} retires in ${retirementAge} years`
+    //  }
+    // console.log(yearUntilRetirement(1997, 'Jenia')) 
+    // console.log(yearUntilRetirement(1978, 'Viktoria')) 
+
+   // const scoreDolphins = (97 + 112 + 101) / 3
+   // const scoreKoalas = (109 + 95 + 123) / 3
+   // console.log(scoreDolphins,scoreKoalas)
+   // if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+   //     console.log("Dolphins win the trophy")
+   // } else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+   //     console.log("Koalas win the trophy")
+   // } else if (scoreDolphins === scoreKoalas) {
+   //     console.log("both win a trophy")
+   // } 
+    //   const calcAge2 = function(birthYear) {
+   //      return 2037 - birthYear;
+   //   }
+   //   const age2 = calcAge2(2004)
+   //   console.log(age1, age2); 
+   
+   
+   
+   
+   
+   // arrow function 
+   //   birthYear => 2037 - birthYear
+   //   const calcAge3 = birthYear => 2037 - birthYear
+   //   const age3 = calcAge3(2037)
+   //   console.log(age3)
+   //   const yearUntilRetirement = (birthYear, firstName) => {
+   //    const age = 2023 - birthYear
+   //    const retirementAge = 65 - age
+   //    // return retirement
+   //    return `${firstName} retires in ${retirementAge} years`
+   //   }
+   //  console.log(yearUntilRetirement(1997, 'Jenia')) 
+   //  console.log(yearUntilRetirement(1978, 'Viktoria')) 
+
+      // const calcAverage = (a, b, c) => (a + b + c) / 3;
+      // console.log(calcAverage(44, 23, 71));
+      // let scoreDolphins = calcAverage(44, 23, 71);
+      // let scoreKoalas = calcAverage(65, 54, 49);
+      // console.log(scoreDolphins,scoreKoalas);
+
+      // const ckeckWinner = function (avgDolphins,avgKoalas) {
+      //    if (avgDolphins >= 2* avgKoalas){
+      //       console.log(`Dolphins win a trophy Dolphins (${avgDolphins} vs ${avgKoalas})`)
+      //    } else if (avgKoalas >= 2 * avgDolphins) {
+      //       console.log (`Dolphins win a trophy Dolphins (${avgDolphins} vs ${avgKoalas})`)
+      //    } else {
+      //       console.log('no team wins')
+      //    }
+      // }
+      // ckeckWinner(scoreDolphins, scoreKoalas )
+      // ckeckWinner(589, 53);
+      // // Test 2
+      //  scoreDolphins = calcAverage(85, 54, 41);
+      //  scoreKoalas = calcAverage(44, 23, 71);
+      //  console.log(scoreDolphins,scoreKoalas);
+      //  ckeckWinner(scoreDolphins, scoreKoalas)
+
+   // const friends = ['Michael', 'Steven', 'Peter'];
+   // console.log(friends[2])
+
+   // const years = new Array(1001,1992, 2022)
+  
+   // friends[2] = 'Jay'
+   // const firstNmae = 'Jonas'
+   // const jonas = [firstNmae, 'sCHMEN', 2037 - 1991, 'teacher', friends];
+   // console.log(jonas);
+
+   // const calcAge = function(birthYear) {
+   //       return 2037 - birthYear;
+   //    }
+   //    const year = [1990, 2292, 2112, 2115, 2030];
+      
+   //    const age1 = calcAge(year[0]);
+   //    const age2 = calcAge(year[1]);
+   //    const age3 = calcAge(year[year.length - 1]);
+   //    console.log(age1, age2, age3);
+
+   //    const ages = [calcAge(year[0]), calcAge(year[1]),
+   //    calcAge(year[years.length - 1]) ];
+   //    console.log(ages);
+//    const friends = ['Michael', 'Steven', 'Peter'];
+//    // Add aelements to array
+//    const newLength = friends.push('Michael');
+//    console.log(friends)
+
+//    friends.unshift('John');
+//    console.log(friends)
+
+//    // Remove elements
+//    friends.pop()
+//    const popepd = friends.pop();
+//    console.log(popepd)
+//    console.log(friends)
+
+//    friends.shift();
+//    console.log(friends)
+
+// console.log(friends.indexOf('Steven'))
+
+// console.log(friends.includes('Bob'));
+
+// const tip = 301
+// const bill = 275
+// const calc = tip >= 50  &&  300 >= tip ? "15 prec": "is 20 prec"
+// console.log (`The bill was ${bill} and tip was ${tip} and the total value is ${tip + bill}`)
+
+const calcTip = function (bill) {
+return bill >= 50 && bill <= 300 ? bill * 0.15 : 
+bill * 0.2
+}
+const bills = [125, 555, 44];
+
+const tip = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])] ;
+console.log(bills, tip) 
+
+const total = [bills[0] + tip [0], bills[1] + tip [1], bills[2] + tip [2]];
+console.log(total);
