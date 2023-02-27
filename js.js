6ebd6d6e0d0df3c7f6644ffc6f8b89c0608444ab
Jenya <78879772+JenyaNoda@users.noreply.github.com>
2023-02-27 18:46:26 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,17 +1,24 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const resButton = document.getElementById("resButton");
-    resButton.addEventListener("click", () => {
-        var containerApp = document.getElementById("container-app");
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     const resButton = document.getElementById("resButton");
+//     resButton.addEventListener("click", () => {
+//         var containerApp = document.getElementById("container-app");
 
-        let v = parseInt(document.getElementById("speed").value);
-        let s = parseInt(document.getElementById("distance").value);
+//         let v = parseInt(document.getElementById("speed").value);
+//         let s = parseInt(document.getElementById("distance").value);
 
-        if (v) {
-            let time = s/v;
-            containerApp.setHTML("Result:" + time);
-        }
-        else {
-            containerApp.setHTML("Error by devined by zero");
-        }
-    });
-});
+//         if (v) {
+//             let time = s/v;
+//             containerApp.setHTML("Result:" + time);
+//         }
+//         else {
+//             containerApp.setHTML("Error by devined by zero");
+//         }
+//     });
+// });
+
+const apple = {
+    a: 10,
+    b: true,
+}
+const copyOfApple = apple
+copyOfApple.c
