853657c72f9bc8f752046b5a24fde3e9e7dcafcf
Jenya <78879772+JenyaNoda@users.noreply.github.com>
2023-03-02 17:26:38 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,24 +1,67 @@
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     const resButton = document.getElementById("resButton");
-//     resButton.addEventListener("click", () => {
-//         var containerApp = document.getElementById("container-app");
+// console.log('bEFORE CONDITION');
+// let random = Math.random()
+// if( random < 1) {
+//     console.log('dwdqdd')
+// // } else console.log ('i dont know how to count')
+//  let passport = 5
+//  let docs = 10;
+//  if (passport && docs >= 15) {
+//     console.log('Thank you, you just finish passportncontrol')
+//  } else if (passport && docs  <= 11){
+//     console.log('Sorry, i need all documents')
+//  } else  {
+//     console.log('log')
+//  }
+ 
+// const practice = function(a,b) {
+// let c 
+//    a = 4
+//    b = 1
+//    c = a + b
+//    if (a <= 100 || c >= 100){
+//       return true;
 
-//         let v = parseInt(document.getElementById("speed").value);
-//         let s = parseInt(document.getElementById("distance").value);
+//    } else {
+//       return false
+//    }
 
-//         if (v) {
-//             let time = s/v;
-//             containerApp.setHTML("Result:" + time);
-//         }
-//         else {
-//             containerApp.setHTML("Error by devined by zero");
-//         }
-//     });
-// });
+// }
+// console.log(practice())
+// const isEqualto100 = (a,b) => a === 100 || b === 100 || (a + b) === 100 ;
+// console.log(isEqualto100(100,0))
 
-const apple = {
-    a: 10,
-    b: true,
-}
-const copyOfApple = apple
-copyOfApple.c
+// const formatDte = (date = new Date ()) => {
+//     const days = date.getDate()
+//     const months = date.getMonth() + 1
+//     const years = date.getFullYear()
+//     return `${days} / ${months} / ${years}`
+// }
+// console.log(formatDte())
+// const striii = 'Hello'
+// const tring = function(a,b) {
+//     if (a === 'string') {
+//         return 'Hello World'
+//     } if else (a === striii) {
+//         return a
+//     }
+// }
+
+// const addNew = (str) => str.indexOf (`New`) === 0? str: `New! ${str}`
+// console.log(addNew('New! Offers'))
+// const dayOfWeek = 'Monday';
+// if (dayOfWeek === 'Monday') {
+//     console.log('Ughhh mONDAY') 
+// }  else if (dayOfWeek === 'Saturday') {
+//     console.log('dsadadaerhfd')
+//     let age = 13
+// if (5 >= age) {
+//     console.log('its free')
+// } else if (10  >= age) {
+//     console.log('Price 20 backs')
+// }   else if ( 11>= age) {
+//     console.log('Price 120 backs') 
+// }
+//     else if (14 >= age) {
+//         console.log('Price 121220 backs')
+//     } else console.log ('For you its 10 backs senior')
+ console.log(Math.floor(Math.random() * 10)) 
