41a42203d94636d9bc27bdb5133465c325b01730
Jenya <78879772+JenyaNoda@users.noreply.github.com>
2023-03-04 16:40:12 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,85 +1,16 @@
-// console.log('bEFORE CONDITION');
-// let random = Math.random()
-// if( random < 1) {
-//     console.log('dwdqdd')
-// // } else console.log ('i dont know how to count')
-//  let passport = 5
-//  let docs = 10;
-//  if (passport && docs >= 15) {
-//     console.log('Thank you, you just finish passportncontrol')
-//  } else if (passport && docs  <= 11){
-//     console.log('Sorry, i need all documents')
-//  } else  {
-//     console.log('log')
-//  }
- 
-// const practice = function(a,b) {
-// let c 
-//    a = 4
-//    b = 1
-//    c = a + b
-//    if (a <= 100 || c >= 100){
-//       return true;
-
-//    } else {
-//       return false
-//    }
-
-// }
-// console.log(practice())
-// const isEqualto100 = (a,b) => a === 100 || b === 100 || (a + b) === 100 ;
-// console.log(isEqualto100(100,0))
-
-// const formatDte = (date = new Date ()) => {
-//     const days = date.getDate()
-//     const months = date.getMonth() + 1
-//     const years = date.getFullYear()
-//     return `${days} / ${months} / ${years}`
-// }
-// console.log(formatDte())
-// const striii = 'Hello'
-// const tring = function(a,b) {
-//     if (a === 'string') {
-//         return 'Hello World'
-//     } if else (a === striii) {
-//         return a
-//     }
-// }
-
-// const addNew = (str) => str.indexOf (`New`) === 0? str: `New! ${str}`
-// console.log(addNew('New! Offers'))
-// const dayOfWeek = 'Monday';
-// if (dayOfWeek === 'Monday') {
-//     console.log('Ughhh mONDAY') 
-// }  else if (dayOfWeek === 'Saturday') {
-//     console.log('dsadadaerhfd')
-//     let age = 13
-// if (5 >= age) {
-//     console.log('its free')
-// } else if (10  >= age) {
-//     console.log('Price 20 backs')
-// }   else if ( 11>= age) {
-//     console.log('Price 120 backs') 
-// }
-//     else if (14 >= age) {
-//         console.log('Price 121220 backs')
-//     } else console.log ('For you its 10 backs senior')
-//  console.log(Math.floor(Math.random() * 10)) 
-// const age = 90;
-// if (age < 5) {
-//     console.log('Free')
-// } else if(age < 10) {
-//     console.log('10$')
-// } else if (age < 65) {
-//     console.log('20$')
-// } else {
-//     console.log('Free')
-// }
-// const firstName = prompt('Name')
-// if(!firstName){
-// firstName = prompt('Enter you firstname please')
-// }
-// const age = 45
-// if(!(age >= 0 && age < 5 || age >= 65)) {
-//     console.log('Free')
-// }
+function buttonColor(){
+    document.getElementById("td").style.backgroundColor="#00FF00"
+     
+}
+function buttonColor1(){
+    document.getElementById("td1").style.backgroundColor="#00FF00"
+}
+function buttonColorq(){
+    document.getElementById("q").style.backgroundColor="#00FF00"
+}
+function buttonColorw(){
+    document.getElementById("w").style.backgroundColor="#00FF00"
+}
+function buttonColore(){
+    document.getElementById("e").style.backgroundColor="#00FF00"
+}
